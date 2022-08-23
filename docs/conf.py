8c3f5c4d55b7665fb22de046f26de77d65596142
6hassan <44853147+6hassan@users.noreply.github.com>
2022-08-23 12:27:12 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@
 import os, sys
 # sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('..\\yclib'))
+sys.path.insert(0, os.path.abspath('..\\sphinx\\yclib'))
 
 project = 'sphinx'
 copyright = '2022, hassan'
