60703459854394ce5cc9a8316291ed864b2e58bc
unknown <6hassan@gmail.com>
2022-08-23 13:08:16 +1000

    task f l

--- yclib/task.py
+++ yclib/task.py
@@ -1,17 +1,17 @@
-# import luigi
+import luigi
 
 def l():
     """l function"""
     return None
 
-# class A(luigi.Task):
-#     """
-#     Class A Descriptionssssss
-#     """
-#     def output(self):
-#         """Output of A"""
-#         return luigi.LocalTarget('D:\SL\sphinx\yclib\task.py')
+class A(luigi.Task):
+    """
+    Class A Descriptionssssss
+    """
+    def output(self):
+        """Output of A"""
+        return luigi.LocalTarget('D:\SL\sphinx\yclib\task.py')
 
-#     def run(self):
-#         """Run A"""
-#         pass
+    def run(self):
+        """Run A"""
+        pass
